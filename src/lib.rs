8a47df2beb888378c62ff7efc98eabd4d134ed45
painternovice <painternovice@gmail.com>
2025-05-03 23:18:52 +0100

    Trying to fix gh pages deployment

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use leptos::prelude::*;
+use leptos::svg::view;
 use leptos_meta::*;
 use leptos_router::{components::*, path};
-use leptos::svg::view;
 
 // Modules
 mod components;
@@ -9,8 +9,27 @@ mod pages;
 
 // Top-Level pages
 use crate::pages::home::Home;
-use crate::pages::timer::Timer;
 use crate::pages::not_found::NotFound;
+use crate::pages::timer::Timer;
+
+pub fn shell(options: LeptosOptions) -> impl IntoView {
+    view! {
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <meta charset="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <AutoReload options=options.clone() />
+                <HydrationScripts options=options islands=true />
+                <link rel="stylesheet" id="leptos" href="/pkg/islands.css" />
+                <link rel="shortcut icon" type="image/ico" href="/favicon.ico" />
+            </head>
+            <body>
+                <App />
+            </body>
+        </html>
+    }
+}
 
 /// An app router which renders the homepage and handles 404's
 #[component]
@@ -29,7 +48,7 @@ pub fn App() -> impl IntoView {
         <Meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
         <Router>
-            <Routes fallback=NotFound >
+            <Routes fallback=NotFound>
                 <Route path=path!("/") view=Home />
                 <Route path=path!("/timer") view=Timer />
             </Routes>
